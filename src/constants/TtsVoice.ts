d0c6cf62a4fc69809930cdca6b634b5f4a25dcdc
syeung <syeung@cvent.com>
2022-12-04 20:34:27 -0500

    add voice constant file

---
+++ src/constants/TtsVoice.ts
@@ -0,0 +1,11 @@
+export const availableVoices = {
+  'Amy': 'Amy',
+  'Joanna': 'Joanna',
+  'Matthew': 'Matthew',
+  'WavenetH': 'en-US-Wavenet-H',
+  'Neural2F': 'en-US-Neural2-F',
+  'WavenetB': 'en-US-Wavenet-B',
+  'WavenetG': 'en-US-Wavenet-G',
+  'WavenetA': 'en-US-Wavenet-A',
+  'WavenetF': 'en-US-Wavenet-F',
+};
